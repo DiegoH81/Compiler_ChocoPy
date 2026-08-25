759353c331becc9cba92fe78a22892bd87f4d489
DiegoH81 <hidal8909ins@gmail.com>
2026-08-25 12:13:03 -0500

    feat(token): add print

--- include/Token.h
+++ include/Token.h
@@ -1,12 +1,13 @@
 #ifndef TOKEN_H
 #define TOKEN_H
 
+#include <iostream>
 #include <string>
 
 enum TokenType
 {
 	// Physical lines
-	EOF,
+	EOFL,
 	EOL,
 
 	// Logical lines
@@ -93,6 +94,87 @@ class Token
 public:
 	TokenType token;
 	std::string lexeme;
+
+	Token(TokenType inToken, const std::string& inLexeme):
+		token(inToken), lexeme(inLexeme)
+	{}
+
+	void print()
+	{
+		std::cout << "< " << tokenTypeToStream(token) << ", " << lexeme << " >\n";
+	}
+private:
+	std::string tokenTypeToStream(TokenType inToken)
+	{
+		switch (inToken)
+		{
+		case EOFL: return "EOFL";
+		case EOL: return "EOL";
+		case NEW_LINE: return "NEW_LINE";
+		case HASHTAG: return "HASHTAG";
+		case INDENT: return "INDENT";
+		case DEDENT: return "DEDENT";
+		case IDENTIFIER: return "IDENTIFIER";
+		case INTEGER: return "INTEGER";
+		case STRING: return "STRING";
+		case KEYWORD_FALSE: return "KEYWORD_FALSE";
+		case KEYWORD_NONE: return "KEYWORD_NONE";
+		case KEYWORD_TRUE: return "KEYWORD_TRUE";
+		case KEYWORD_AND: return "KEYWORD_AND";
+		case KEYWORD_AS: return "KEYWORD_AS";
+		case KEYWORD_ASSERT: return "KEYWORD_ASSERT";
+		case KEYWORD_ASYNC: return "KEYWORD_ASYNC";
+		case KEYWORD_AWAIT: return "KEYWORD_AWAIT";
+		case KEYWORD_BREAK: return "KEYWORD_BREAK";
+		case KEYWORD_CLASS: return "KEYWORD_CLASS";
+		case KEYWORD_CONTINUE: return "KEYWORD_CONTINUE";
+		case KEYWORD_DEF: return "KEYWORD_DEF";
+		case KEYWORD_DEL: return "KEYWORD_DEL";
+		case KEYWORD_ELIF: return "KEYWORD_ELIF";
+		case KEYWORD_ELSE: return "KEYWORD_ELSE";
+		case KEYWORD_EXCEPT: return "KEYWORD_EXCEPT";
+		case KEYWORD_FINALLY: return "KEYWORD_FINALLY";
+		case KEYWORD_FOR: return "KEYWORD_FOR";
+		case KEYWORD_FROM: return "KEYWORD_FROM";
+		case KEYWORD_GLOBAL: return "KEYWORD_GLOBAL";
+		case KEYWORD_IF: return "KEYWORD_IF";
+		case KEYWORD_IMPORT: return "KEYWORD_IMPORT";
+		case KEYWORD_IN: return "KEYWORD_IN";
+		case KEYWORD_IS: return "KEYWORD_IS";
+		case KEYWORD_LAMBDA: return "KEYWORD_LAMBDA";
+		case KEYWORD_NONLOCAL: return "KEYWORD_NONLOCAL";
+		case KEYWORD_NOT: return "KEYWORD_NOT";
+		case KEYWORD_OR: return "KEYWORD_OR";
+		case KEYWORD_PASS: return "KEYWORD_PASS";
+		case KEYWORD_RAISE: return "KEYWORD_RAISE";
+		case KEYWORD_RETURN: return "KEYWORD_RETURN";
+		case KEYWORD_TRY: return "KEYWORD_TRY";
+		case KEYWORD_WHILE: return "KEYWORD_WHILE";
+		case KEYWORD_WITH: return "KEYWORD_WITH";
+		case KEYWORD_YIELD: return "KEYWORD_YIELD";
+		case DELIM_LPAREN: return "DELIM_LPAREN";
+		case DELIM_RPAREN: return "DELIM_RPAREN";
+		case DELIM_LBRACKET: return "DELIM_LBRACKET";
+		case DELIM_RBRACKET: return "DELIM_RBRACKET";
+		case DELIM_COMMA: return "DELIM_COMMA";
+		case DELIM_COLON: return "DELIM_COLON";
+		case DELIM_DOT: return "DELIM_DOT";
+		case OP_PLUS: return "OP_PLUS";
+		case OP_MINUS: return "OP_MINUS";
+		case OP_STAR: return "OP_STAR";
+		case OP_SLASH_SLASH: return "OP_SLASH_SLASH";
+		case OP_PERCENT: return "OP_PERCENT";
+		case OP_LT: return "OP_LT";
+		case OP_GT: return "OP_GT";
+		case OP_LE: return "OP_LE";
+		case OP_GE: return "OP_GE";
+		case OP_EQEQ: return "OP_EQEQ";
+		case OP_NEQ: return "OP_NEQ";
+		case OP_ASSIGN: return "OP_ASSIGN";
+		case OP_ARROW: return "OP_ARROW";
+		default: return "UNKNOWN_TOKEN";
+		}
+	}
 };
 
 
